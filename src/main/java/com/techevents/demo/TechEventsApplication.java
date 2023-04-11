5942b1dc52be2f33859d0a5dcd1644f74e4de33b
Pere1J <pere1josep@gmail.com>
2023-04-11 14:53:37 +0200

    first commit

---
+++ src/main/java/com/techevents/demo/TechEventsApplication.java
@@ -0,0 +1,13 @@
+package com.techevents.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechEventsApplication.class, args);
+	}
+
+}
