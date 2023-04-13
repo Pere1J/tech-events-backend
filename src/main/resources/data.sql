4599942bddde1a2353a5dea1aef9b4612e9bae13
javier <villarestrepocarlos@gmail.com>
2023-04-13 19:40:03 +0200

    New Eventos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -28,7 +28,8 @@ INTO
   events
   (url_image, title, degree, event_date, event_time, capacity,  highlight, description, category_id)
 VALUES
-  ('https://www.digitallearning.es/wp-content/uploads/2017/11/PosterCurso_IniciacionProgamacionJS.png', 'Taller de iniciacion a la programacion', 'Beginner', '2023-05-20', '09:00', 20, TRUE, 'Si usas herramientas informaticas, quizas te hayas planteado en alguna ocasion aprender a programar para poder abordar nuevos proyectos, mejorar tus competencias profesionales o simplemente como hobby.Si usas herramientas informaticas, quizas te hayas planteado en alguna ocasion aprender a programar para poder abordar nuevos proyectos, mejorar tus competencias profesionales o simplemente como hobby.Si usas herramientas informaticas, quizas te hayas planteado en alguna ocasion aprender a programar para poder abordar nuevos proyectos, mejorar tus competencias profesionales o simplemente como hobby.', 2);
+  ('https://i.blogs.es/164b03/curso-programacion/1366_2000.jpg', 'Webinar Buenas Prácticas en Desarrollo de Aplicaciones con React', 'Intermediate', '2023-05-20', '09:00', 50, TRUE,
+  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 2);
 
 
 INSERT
@@ -36,14 +37,86 @@ INTO
   events
   (url_image, title, degree, event_date, event_time, capacity,  highlight, description, category_id)
 VALUES
-  ('https://i.ytimg.com/vi/1xFscRkO8QU/maxresdefault.jpg', 'Curso Intermedio Programacion CNC | CMZ Academy', 'Intermediate', '2023-06-20', '19:00', 10, TRUE, 'Curso CNC orientado a personas con experiencia en el manejo de tornos control numerico pero que desean aprender o aumentar sus conocimientos con la herramien...', 3);
+  ('https://i.ytimg.com/vi/1xFscRkO8QU/maxresdefault.jpg', 'Workshop de Programacion CNC | CMZ Academy', 'Intermediate', '2023-06-20', '19:00', 30, FALSE,
+  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 1);
 
 INSERT
 INTO
   events
   (url_image, title, degree, event_date, event_time, capacity, highlight,description, category_id)
 VALUES
-  ('https://i.ytimg.com/vi/1xFscRkO8QU/maxresdefault.jpg', 'Curso Avanzado de Programacion','Advanced', '2023-02-01', '08:00', 100, TRUE,'This course teaches additional skills and techniques to programmers who can already write comprehensive RPG IV programs.                       This class offers a comprehensive discussion of some of the advanced features and functions of RPG IV. This class is designed to enable an experienced RPG IV programmer to develop and maintain RPG IV programs of an advanced level using the latest features and techniques available in the IBM i RPG IV compiler.', 1);
+  ('https://assets2-programarya-com.juandmegon.com/img/Cursos-Avanzados/Peticiones-HTTP-con-Laravel/icon.png', 'Master Class Desarrollo de Aplicaciones Web Escalables con Node.js y Express','Advanced', '2023-07-15', '08:00', 20, FALSE,
+  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 3);
+---
+INSERT
+INTO
+  events
+  (url_image, title, degree, event_date, event_time, capacity, highlight,description, category_id)
+VALUES
+  ('https://databits.ai/wp-content/uploads/2020/02/Miniatura-01.png', '"Master class Introducción a la Programación en Python','Beginner', '2023-04-15', '08:30', 50, FALSE,
+  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 3);
 
+INSERT
+INTO
+  events
+  (url_image, title, degree, event_date, event_time, capacity, highlight,description, category_id)
+VALUES
+  ('https://assets2-programarya-com.juandmegon.com/img/Cursos-Avanzados/Peticiones-HTTP-con-Laravel/icon.png', 'Webinar fundamentos de Java','Beginner', '2023-04-01', '08:30', 50, TRUE,
+  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 2);
+------
+INSERT
+INTO
+  events
+  (url_image, title, degree, event_date, event_time, capacity, highlight,description, category_id)
+VALUES
+  ('https://tincode-django.s3.amazonaws.com/media/learning_careers/e11ed32a-2ca9-4654-9495-9a0a59abf270.jpg', 'Work Shop Desarrollo de Aplicaciones Móviles con React Native','Intermediate', '2023-05-07', '08:00', 30, FALSE,
+  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 1);
 
+INSERT
+INTO
+  events
+  (url_image, title, degree, event_date, event_time, capacity, highlight,description, category_id)
+VALUES
+  ('https://assets2-programarya-com.juandmegon.com/img/Cursos-Avanzados/Peticiones-HTTP-con-Laravel/icon.png', 'Master Class Desarrollo de Aplicaciones Web con MEAN Stack','Intermediate', '2023-08-01', '08:00', 30, FALSE,
+  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 3);
+------
 
+INSERT
+INTO
+  events
+  (url_image, title, degree, event_date, event_time, capacity, highlight,description, category_id)
+VALUES
+  ('https://coderslink.com/wp-content/uploads/2021/02/Primeros-pasos-en-Machine-Learning-con-TensorFlow.jpg', 'Work Shop Inteligencia Artificial con TensorFlow','Advanced', '2023-07-04', '09:00', 20, TRUE,
+  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 1);
+
+INSERT
+INTO
+  events
+  (url_image, title, degree, event_date, event_time, capacity, highlight,description, category_id)
+VALUES
+  ('https://www.gb-advisors.com/wp-content/uploads/2018/10/Seguridad-de-aplicaciones-web-4-2.png', 'Webinar Seguridad en Aplicaciones Web','Advanced', '2023-06-25', '17:00', 20, FALSE,
+  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 2);
+
+  INSERT
+  INTO
+    events
+    (url_image, title, degree, event_date, event_time, capacity, highlight,description, category_id)
+  VALUES
+    ('https://www.azulweb.net/wp-content/uploads/2020/05/videojuegos-unity-3d-ATCONMASFUTURO17-18-1.png', 'Master Class Desarrollo de Juegos en Unity','Advanced', '2023-07-07', '09:30', 20, TRUE,
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 3);
+
+  INSERT
+  INTO
+    events
+    (url_image, title, degree, event_date, event_time, capacity, highlight,description, category_id)
+  VALUES
+    ('https://i.ytimg.com/vi/5sapd1rsGbs/maxresdefault.jpg', 'Webinar Desarrolo de videojuegos con Unity','Beginner', '2023-06-25', '09:00', 30, TRUE,
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 2);
+
+  INSERT
+  INTO
+    events
+    (url_image, title, degree, event_date, event_time, capacity, highlight,description, category_id)
+  VALUES
+    ('https://factor3d.com/wp-content/uploads/2020/07/La-gu%C3%ADa-definitiva-del-desarrollo-de-videojuegos-con-Unity.jpg', 'Work Shop Desarrolo de videojuegos con Unity','Intermediate', '2023-07-01', '09:00', 30, TRUE,
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 1);
