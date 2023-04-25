4835d0c42d2b7ebbb2b27e95b601cd1ee0266756
Pere1J <pere1josep@gmail.com>
2023-04-25 08:49:21 +0200

    added send mail to inscribed

--- src/main/java/com/techevents/service/InscribedUserService.java
+++ src/main/java/com/techevents/service/InscribedUserService.java
@@ -25,7 +25,7 @@ public class InscribedUserService {
     public void inscribedEvent(Long eventId){
         var event = eventRepository.findById(eventId).orElseThrow(() ->new RuntimeException("event does not exist"));
         var auth = authFacade.getAuthUser();
-        if (event.isUserInscribed(auth))return; // crear una respuesta + modo de desinscribirse.
+        if (event.isUserInscribed(auth))return;
         var inscribedUser =  new InscribedUser(auth, event);
         inscribedUserRepository.save(inscribedUser);
 
